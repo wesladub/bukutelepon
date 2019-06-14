dd3479ce796bd2177843fdae9ddb46ac9cb388fc
GiriD <GiriD@GiriD-PC>
2019-06-14 08:52:36 +0700

    Add, Show, Update, Delete, Search

--- src/responsi2/responsi.java
+++ src/responsi2/responsi.java
@@ -16,7 +16,7 @@ public class responsi {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Scanner str = new Scanner(System.in);
-        int pil = 0, jumdat = 0, ubah = 0;
+        int pil = 0, jumdat = 0, ubah = 0, s = 0, hapus = 0;
         String data[][] = new String[50][4];
 
         do {
@@ -25,7 +25,8 @@ public class responsi {
             System.out.println("** 2. MENAMPILKAN DATA      **");
             System.out.println("** 3. MENGUBAH DATA         **");
             System.out.println("** 4. MENGHAPUS DATA        **");
-            System.out.println("** 5. KELUAR                **");
+            System.out.println("** 5. CARI DATA             **");
+            System.out.println("** 6. KELUAR                **");
             System.out.println("******************************");
             System.out.print("Masukkan Pilihan Anda : ");
             pil = input.nextInt();
@@ -67,13 +68,13 @@ public class responsi {
                             }
                             System.out.println(data[i][j]);
                         }
-                        System.out.println("=============================");
+                        System.out.println("===============================");
                     }
                     break;
                 case 3:
                     System.out.print("Silahkan pilih nomor data yang akan diubah : ");
                     ubah = input.nextInt();
-                    int s = 0;
+                    System.out.println("\n=============================");
                     System.out.printf("%-16s %-1s", "Masukkan Nama", ": ");
                     data[ubah - 1][s] = str.nextLine();
                     System.out.printf("%-16s %-1s", "Masukkan Alamat", ": ");
@@ -85,10 +86,52 @@ public class responsi {
                     System.out.println("=============================");
                     System.out.println("    Data berhasil diubah     ");
                     break;
+                case 4:
+                    System.out.println("======Hapus data======");
+                    System.out.print("Masukkan nomor data yang ingin dihapus : ");
+                    hapus = input.nextInt();
+                    data[hapus - 1][s] = "";
+                    data[hapus - 1][s + 1] = "";
+                    data[hapus - 1][s + 2] = "";
+                    data[hapus - 1][s + 3] = "";
+                    System.out.println("=============================");
+                    System.out.println("    Data berhasil dihapus    ");
+                    break;
+                case 5:
+                    System.out.println("=======Cari data=======");
+                    System.out.print("Cari : ");
+                    String cari = str.nextLine();
+                    for (int z = 0; z < jumdat; z++) {
+                        for (int q = 0; q < 1; q++) {
+                            if (data[z][q].matches("(.*)" + cari + "(.*)") == true) {
+
+                                System.out.println("============= " + (z + 1) + " ===============");
+                                for (int j = 0; j < 4; j++) {
+                                    switch (j) {
+                                        case 0:
+                                            System.out.printf("%-7s %-1s", "Nama", ": ");
+                                            break;
+                                        case 1:
+                                            System.out.printf("%-7s %-1s", "Alamat", ": ");
+                                            break;
+                                        case 2:
+                                            System.out.printf("%-7s %-1s", "No.Telp", ": ");
+                                            break;
+                                        case 3:
+                                            System.out.printf("%-7s %-1s", "Email", ": ");
+                                            break;
+                                    }
+                                    System.out.println(data[z][j]);
+                                }
+                                System.out.println("=============================");
+                            } 
+                        }
+                    }
+                    break;
             }
             System.out.println("");
 
-        } while (pil != 5);
+        } while (pil != 6);
         System.out.println("Terima Kasih");
     }
 }
